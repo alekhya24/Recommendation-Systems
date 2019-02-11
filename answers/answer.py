f7baa315abce3c2e0261a3af038da8e97cdfded2
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-10 22:05:47 -0500

    Test Query1 V1.0.2

--- answers/answer.py
+++ answers/answer.py
@@ -85,9 +85,8 @@ def basic_als_recommender(filename, seed):
 
     # Build the recommendation model using ALS on the training data
     # Note we set cold start strategy to 'drop' to ensure we don't get NaN evaluation metrics
-    als = ALS(rank=70,maxIter=5, regParam=0.01,seed=seed,coldStartStrategy="drop")
+    als = ALS(rank=70,maxIter=5, regParam=0.01,seed=seed,userCol="userId", itemCol="movieId", ratingCol="rating",coldStartStrategy="drop")
     als.setSeed(seed)
-    '''userCol="userId", itemCol="movieId", ratingCol="rating",'''
     model = als.fit(training)
 
     # Evaluate the model by computing the RMSE on the test data
