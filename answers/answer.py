a0c1e3092b6484c3b718e4142f99ab0b540a7acb
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-12 02:55:17 -0500

    Test Q4 V1.1.16

--- answers/answer.py
+++ answers/answer.py
@@ -206,7 +206,7 @@ def means_and_interaction(filename, seed, n):
     # use map() to calculate mean rating of each user
     user_meanRating = user_sumRating_numRating.mapValues(lambda sum_rating, num_rating:
     sum_rating / num_rating)
-    print user_meanRating.take(5)
+    user_meanRating.collect().foreach { x => println(x)}
     '''for v in user_meanRating:
         print("all_user_mean:{0}".format(v))'''
     return []
