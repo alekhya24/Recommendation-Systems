c5936bd56b9d4191eb4f317095899521fcf91366
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-11 23:01:08 -0500

    Test Q4 V1.0.12

--- answers/answer.py
+++ answers/answer.py
@@ -187,8 +187,8 @@ def means_and_interaction(filename, seed, n):
     predictions = model.transform(test)
     evaluator = RegressionEvaluator(metricName="mean", labelCol="rating",
                                 predictionCol="prediction")'''
-    each_user_mean = training.groupBy("userId").agg({"rating":"avg"}).alias("rating")
-    all_user_mean=each_user_mean.agg({"rating":"avg"}).collect()
+    each_user_mean = training.groupBy("userId").agg({"rating":"avg"})
+    all_user_mean=each_user_mean.agg({"avg(rating)":"avg"}).collect()
     print("each_user_mean:{0}".format(each_user_mean))
     print("all_user_mean:{0}".format(all_user_mean))
     return []
