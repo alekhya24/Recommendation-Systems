d18f9c4ee62098c08d200544c6bd2aa0b71e56f2
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-12 02:59:02 -0500

    Test Q4 V1.1.17

--- answers/answer.py
+++ answers/answer.py
@@ -206,9 +206,8 @@ def means_and_interaction(filename, seed, n):
     # use map() to calculate mean rating of each user
     user_meanRating = user_sumRating_numRating.mapValues(lambda sum_rating, num_rating:
     sum_rating / num_rating)
-    user_meanRating.collect().foreach { x => println(x)}
-    '''for v in user_meanRating:
-        print("all_user_mean:{0}".format(v))'''
+    user_meanRating.collect()
+        print("all_user_mean:{0}".format(user_meanRating))
     return []
 
 def als_with_bias_recommender(filename, seed):
