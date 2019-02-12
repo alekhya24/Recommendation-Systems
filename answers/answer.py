7b96cb941533d49599112612a6a16634351dc09a
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-12 04:46:47 -0500

    Q4 V1.2.12

--- answers/answer.py
+++ answers/answer.py
@@ -194,7 +194,7 @@ def means_and_interaction(filename, seed, n):
     all_user_mean=each_user_mean.agg({"avg(rating)":"mean"}).collect()
     print("each_user_mean:{0}".format(each_user_mean))'''
     '''user_rating = training.select(userId, rating)'''
-    user_rating = ratingsRDD.map(lambda x: (x[0], x[1]))
+    user_rating = ratingsRDD.map(lambda x: (x[0], x[2]))
     user_sumRating_numRating = user_rating.combineByKey(
     # start with the first rating and set count to one​
     createCombiner=lambda first_rating: (first_rating, 1),
