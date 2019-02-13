59b55a4883050752424099fa15ba44700894dc2d
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-12 23:15:34 -0500

    Q4 V1.5.4

--- answers/answer.py
+++ answers/answer.py
@@ -8,7 +8,7 @@ from pyspark.sql.functions import lit
 from pyspark.sql.functions import desc
 from pyspark.ml.evaluation import RegressionEvaluator
 from pyspark.ml.recommendation import ALS
-from pyspark.sql.functions import col
+from pyspark.sql.types import StructType
 '''os.environ['HADOOP_HOME'] = "C:\\winutils"'''
 
 '''
