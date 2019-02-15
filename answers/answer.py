276154195f163f2480304e61fab18e4d865ac977
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-15 00:36:06 -0500

    Q5 V1.0.11

--- answers/answer.py
+++ answers/answer.py
@@ -189,9 +189,10 @@ def means_and_interaction(filename, seed, n):
         user_item_interaction =i.rating-(user_mean+ item_mean - global_mean)
         l = l + [([i.userId,i.movieId,i.rating,user_mean,item_mean,user_item_interaction])]
         temp_df = spark.createDataFrame(l, schema)
-        print(l)
         final_df = final_df.union(temp_df)
-    return final_df.take(n);   
+    for i in final_df.collect()
+        print(i)
+    return final_df.collect();   
 
 def als_with_bias_recommender(filename, seed):
     '''
@@ -205,7 +206,7 @@ def als_with_bias_recommender(filename, seed):
     as before and be initialized with the random seed passed as 
     parameter. Test file: tests/test_als_with_bias_recommender.py
     '''
-    spark=init_spark()
+    '''spark=init_spark()
     lines = spark.read.text(filename).rdd
     parts = lines.map(lambda row: row.value.split("::"))
     ratingsRDD=parts.map(lambda p: Row(userId=int(p[0]), movieId=int(p[1]),
@@ -245,4 +246,5 @@ def als_with_bias_recommender(filename, seed):
 
     # Root mean squared error
     print("RMSE = %s" % metrics.rootMeanSquaredError)
-    return metrics.rootMeanSquaredError
+    return metrics.rootMeanSquaredError'''
+    return 0
