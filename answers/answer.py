357d5dbee53d0bb67d18d9cf40b63fa19969ccc7
alekhya24 <alekhya.karicharla9@gmail.com>
2019-02-16 01:09:00 -0500

    Commit 2.1

--- answers/answer.py
+++ answers/answer.py
@@ -191,7 +191,7 @@ def means_and_interaction(filename, seed, n):
     temp_df = spark.createDataFrame(l, schema)
     final_df = final_df.union(temp_df)'''
     
-    training_with_means=op_df.withColumn("user_mean",lit(getUserMean(each_user_mean,userId)))
+    training_with_means=op_df.withColumn("user_mean",lit(getUserMean(each_user_mean,'userId')))
     '''.withColumn("item_mean",lit(getItemMean(each_item_mean,op_df.movieId)))
     final_df = training_with_means.withColumn("user_item_interaction",lit(calculate_interaction(training_with_means.rating,training_with_means.user_mean,
                                                                                                                       training_with_means.item_mean,global_mean)))'''
